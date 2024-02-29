1c7575ceb57a5af0f576e064b39af6180e7b9cd5
JeanL19 <156533956+JeanL19@users.noreply.github.com>
2024-02-29 11:55:22 -0500

    Cambio en el formato del codigo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from services.clientes import getclients, generateTableHTML, getClientsById, createClient, updateClient, deleteClient
 from config.db import cursor, conexion
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,41 +9,24 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/clientes")
+@app.get("/clientes", response_class=HTMLResponse)
 async def obtener_clientes():
-    cursor.execute("SELECT * FROM clientes")
-    clientes = cursor.fetchall()
-    return clientes
+    clients = getclients(cursor)
+    return generateTableHTML(clients)
 
-@app.get("/clientes/{name}")
-async def obtener_clientes(name: str):
-    cursor.execute("SELECT * FROM clientes WHERE Nombre = '"+str(name)+"'")
-    clientes = cursor.fetchall()
-    return clientes
-
-@app.get("/clientes/{id}") ### no funciona aun si puedes buscale solucion porfis
-async def obtener_clientes(id: int):
-    cursor.execute("SELECT * FROM clientes WHERE id = '"+str(id)+"'")
-    clientes = cursor.fetchall()
-    return clientes
+@app.get("/clientes/{id}")
+async def obtener_clientes(id:int):
+    resultadoCliente = getClientsById(cursor, id)
+    return resultadoCliente
 
 @app.post("/crear-cliente")
-async def crear_cliente(Nombre: str, Contacto: str, Fecha_ingreso: str, Tipo: str, Marca: str, Modelo: str, Diagnostico: str, Estado: str):
-    cursor.execute("INSERT INTO clientes (Nombre, Contacto, Fecha_ingreso, Tipo, Marca, Modelo, Diagnostico, Estado) VALUES ('"+Nombre+"', '"+str(Contacto)+"', '"+Fecha_ingreso+"', '"+Tipo+"', '"+Marca+"', '"+Modelo+"', '"+Diagnostico+"', '"+Estado+"' )")
-    conexion.commit()
-    return {"mensaje": "Cliente creado correctamente"}
+async def   crear_cliente(Nombre: str, Contacto: str, Fecha_ingreso: str, Tipo: str, Marca: str, Modelo: str, Diagnostico: str, Estado: str):
+    return createClient(cursor, conexion, Nombre, Contacto, Fecha_ingreso, Tipo, Marca, Modelo, Diagnostico, Estado)
 
 @app.put("/actualizar-cliente/{name}")
-async def actualizar_cliente(name: str, newEstado: str):
-    cursor.execute("UPDATE clientes SET Estado = '"+newEstado+"' WHERE Nombre = '"+str(name)+"'")
-    conexion.commit()
-    return {"mensaje": "cliente actualizado correctamente"}
+async def actualizar_cliente(name:str, newName: str):
+    return updateClient(cursor, conexion, name, newName)
 
-@app.delete('/eliminar_cliente/{id}')
-async def eliminar_cliente(id:int):
-    cursor.execute("DELETE FROM clientes WHERE id = '"+str(id)+"'")
-    conexion.commit()
-    return {"messaje": "Cliente Eliminado Correctamente"}
-
-
-
+@app.delete("/Eliminar-cliente/{id}")
+async def eliminar_Clente(id: int):
+    return deleteClient(cursor, conexion, id) 
